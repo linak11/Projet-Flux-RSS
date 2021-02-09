72743dd248a45713aaae346e094b8d60eb8ddd6f
zaharat <zaharat_b@hotmail.fr>
2021-02-09 09:30:35 +0100

    ajout de fichiers

--- index.php
+++ index.php
@@ -4,14 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Flux RSS</title>
+    <title>FIDZ</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
     <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="shortcut icon" type="image/x-icon" href="/assets/img/favicon.ico">
+
 </head>
-<body>
-    <nav class="navbar navbar-expand-lg">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Logo</a>
+<body>   
+    <nav class="navbar navbar-expand-lg sticky-top">
+    <div class="container-fluid" >
+          <a class="navbar-brand" href="#"><img src="/assets/img/logo.png" alt="Logo du site"></a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
@@ -28,22 +31,78 @@
               </li>
             
             </ul>
-            <form class="d-flex">
-              <input class="form-control me-2" type="search" placeholder="Rechercher sur le site" aria-label="Search">
-              <button class="btn btn-outline-success" type="submit">Rechercher</button>
+            <form class="d-flex my-3">
+              <input class="form-control me-2 ml-1 case" type="search" placeholder="Rechercher sur le site" aria-label="Search">
+              <button class="btn btn-outline-success button ms-3 me-3 " type="submit">Rechercher</button>
             </form>
           </div>
         </div>
       </nav>
 
-      <h1 class="title">FIDZ</h1>
+      <div class="d-flex justify-content-center mt-2">
+      <img src="/assets/img/title.png" class="animate__animated animate__bounce " id="f"/>
+      </div>
+      <div class="row">
+  <div class="col-sm-6 my-3">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Plus de détails</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 my-3">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Plus de détails</a>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="row">
+  <div class="col-sm-6 my-3">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Plus de détails</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 my-3">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Plus de détails</a>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="row">
+  <div class="col-sm-6 my-3">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Plus de détails</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 my-3">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Plus de détails</a>
+      </div>
+    </div>
+  </div>
+</div>
 
-
-
-
-
-
-
-
-    </body>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+</body>
 </html>
